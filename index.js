b188acf26b74d51db8e2964577c2d52ab33d395c
Viktoriia <freshitpte@gmail.com>
2024-09-01 19:20:36 +0300

    Add users crud

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const http = require('node:http');
-const express = require('express');
+const app = require('./app');
 
 const PORT = process.env.PORT ?? 5000;
-
-const app = express();
-
-// button.addRequestListener('event', handler)
-// app.method('/users', hadler)
-
-// GET http://localhost:5000/
-app.get('/', (request, response) => {
-  response.status(200).send('Hello, world))))');
-});
 
 const server = http.createServer(app);
 
